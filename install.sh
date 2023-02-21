95c776a2d5067e519e9368f2d66a52af4c754fdb
keahi32 <50500100+keahi32@users.noreply.github.com>
2023-02-21 15:56:38 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,119 +4,102 @@ echo " "
 echo "-------------------------------"
 echo " "
 echo "Instalacion Automatica de Docker."
-echo "Developed by Jesus Dev."
+echo "Developed by Jesus Dev and g0dsp."
 echo " "
-sleep 1
 echo "-------------------------------"
 echo " "
-sleep 1
+sleep 5
 echo "-------------------------------"
 echo " "
 echo "Haciendo update"
 echo " "
 echo "-------------------------------"
-sleep 1
-sudo apt-get update
-sleep 5
+sudo apt-get update 2>/dev/null
 clear
 echo "-------------------------------"
 echo " "
 echo "Haciendo upgrade"
 echo " "
 echo "-------------------------------"
-sleep 1
-sudo apt-get upgrade -y
-sleep 5
+sudo apt-get upgrade -y 2>/dev/null
 clear
 echo "-------------------------------"
 echo " "
 echo "Instalando Certificados"
 echo " "
 echo "-------------------------------"
-sleep 1
-sudo apt-get install ca-certificates -y
-sleep 5
+sudo apt-get install ca-certificates -y 2>/dev/null
+clear
 echo "-------------------------------"
 echo " "
 echo "Instalando Curl"
 echo " "
 echo "-------------------------------"
-sleep 1
-sudo apt-get install curl -y
-sleep 5
+sudo apt-get install curl -y 2>/dev/null
+clear
 echo "-------------------------------"
 echo " "
 echo "Instalando gnupg"
 echo " "
 echo "-------------------------------"
-sleep 1
-sudo apt-get install gnupg -y
-sleep 5
+sudo apt-get install gnupg -y 2>/dev/null
+clear
 echo "-------------------------------"
 echo " "
 echo "Instalando Libreria"
 echo " "
 echo "-------------------------------"
-sleep 1
-sudo apt-get install lsb-release -y
-sleep 5
+sudo apt-get install lsb-release -y 2>/dev/null
 clear
 echo "-------------------------------"
 echo " "
 echo "Descargando certificados de docker"
 echo " "
 echo "-------------------------------"
-sleep 1
-curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
-sleep 5
+curl -fsSL https://download.docker.com/linux/debian/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg 
+clear
 echo "-------------------------------"
 echo " "
 echo "Instalando Certificados de Docker"
 echo " "
 echo "-------------------------------"
-sleep 1
 echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/debian $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
-sleep 3
+clear
 echo "-------------------------------"
 echo " "
 echo "Actualizando Sistema"
 echo " "
 echo "-------------------------------"
-sleep 1
-apt-get update
-sleep 5
+apt-get update 2>/dev/null
 clear
 echo "-------------------------------"
 echo " "
 echo "Instalando Docker y dependencias"
 echo " "
 echo "-------------------------------"
-sleep 1
-apt-get install docker-ce docker-ce-cli containerd.io -y
-sleep 5
+apt-get install docker-ce docker-ce-cli containerd.io -y 2>/dev/null
+clear
 echo "-------------------------------"
 echo " "
 echo "Creando Volumen de Portainer"
 echo " "
 echo "-------------------------------"
-sleep 1
-docker volume create portainer_data
+docker volume create portainer_data 2>/dev/null
+clear
 echo "-------------------------------"
 echo " "
 echo "Lanzamos nuestro docker de Portainer"
 echo " "
 echo "-------------------------------"
-sleep 1
-docker run -d --name=portainer --hostname=portainer --network=host --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data -e TZ='Europe/Madrid' portainer/portainer-ce 
-sleep 5
+docker run -d --name=portainer --hostname=portainer --network=host --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data -e TZ='Europe/Madrid' portainer/portainer-ce 2>/dev/null
+clear
 echo "-------------------------------"
 echo " "
 echo "Instalando docker de MARIADB"
 echo " "
 echo "-------------------------------"
-sleep 1
-docker container run --name sql-maria -e MYSQL_ROOT_PASSWORD=12345 -e MYSQL_USER=username -e MYSQL_PASSWORD=12345 -e MYSQL_DATABASE=docker -p 3306:3306 -d mariadb:10
-sleep 5
+docker container run --name sql-maria -e MYSQL_ROOT_PASSWORD=12345 -e MYSQL_USER=username -e MYSQL_PASSWORD=12345 -e MYSQL_DATABASE=docker -p 3306:3306 -d mariadb:10 2>/dev/null
+clear
 echo "-------------------------------"
 echo " "
 echo "Enhorabuena Docker + Portainer + MariaDB esta instalado"
